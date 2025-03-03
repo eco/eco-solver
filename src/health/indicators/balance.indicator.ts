e1fa0d3b728158ed4ba44962e69557220699ee80
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-03-03 16:34:07 -0500

    adding the total balance to the health endpoint (#100)

--- src/health/indicators/balance.indicator.ts
+++ src/health/indicators/balance.indicator.ts
@@ -24,7 +24,7 @@ export class BalanceHealthIndicator extends HealthIndicator {
       this.getSolvers(),
       this.getSources(),
     ])
-    let isHealthy = solvers.every((solver) => {
+    let isHealthy = solvers.balances.every((solver) => {
       const tokens = solver.tokens
       return Object.values(tokens).every((token) => {
         if (!token.minBalances) {
@@ -98,15 +98,18 @@ export class BalanceHealthIndicator extends HealthIndicator {
     return sources
   }
 
-  private async getSolvers(): Promise<{ tokens: Record<string, TokenType> }[]> {
-    const solvers: Array<{
+  private async getSolvers(): Promise<{
+    balances: { tokens: Record<string, TokenType> }[]
+    totalBalance: number
+  }> {
+    const solverBalances: Array<{
       accountAddress: `0x${string}` | undefined
       tokens: Record<string, TokenType>
       inboxAddress: Hex
       network: Network
       chainID: number
     }> = []
-
+    let totalBalance = 0
     const solverConfig = this.configService.getSolvers()
     await Promise.all(
       Object.entries(solverConfig).map(async ([, solver]) => {
@@ -119,16 +122,17 @@ export class BalanceHealthIndicator extends HealthIndicator {
         entries(solver.targets).forEach((target) => {
           ;(target[1] as TargetContract & { balance: object }).balance =
             sourceBalancesString[target[0]]
+          totalBalance += parseInt(sourceBalancesString[target[0]].value)
         })
 
-        solvers.push({
+        solverBalances.push({
           ...solver,
           accountAddress,
           tokens: sourceBalancesString,
         })
       }),
     )
-    return solvers
+    return { balances: solverBalances, totalBalance }
   }
 
   private async getBalanceCalls(chainID: number, tokens: Hex[]) {
