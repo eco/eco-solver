216306305474b7e62f2faa090278a3d87fcbe5aa
Carlos Febres <cfebres@eco.com>
2025-03-15 15:40:45 -0400

    update Celo project

--- src/common/chains/definitions/celo.ts
+++ src/common/chains/definitions/celo.ts
@@ -1,16 +1,12 @@
 import { Chain } from 'viem'
 import { celo as vcelo } from 'viem/chains'
-import * as config from 'config'
-import { QuicknodeConfigType } from '@/eco-configs/eco-config.types'
-
-const { apiKey: quickNodeApiKey } = config.get<QuicknodeConfigType>('quicknode')
 
 export const celo: Chain = {
   ...vcelo,
   rpcUrls: {
     default: {
-      http: [`https://ancient-quaint-layer.celo-mainnet.quiknode.pro/{QUICKNODE_API_KEY}`],
-      webSocket: [`wss://ancient-quaint-layer.celo-mainnet.quiknode.pro/{QUICKNODE_API_KEY}`],
+      http: [`https://responsive-lingering-yard.celo-mainnet.quiknode.pro/{QUICKNODE_API_KEY}`],
+      webSocket: [`wss://responsive-lingering-yard.celo-mainnet.quiknode.pro/{QUICKNODE_API_KEY}`],
     },
   },
 }
