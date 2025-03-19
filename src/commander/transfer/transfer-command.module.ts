83044e8ec17df87f0fd09fd0f18a4d9eadd917e3
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-03-19 12:44:01 -0400

    adding wallet address native gas token transfers all chain token transfers

--- src/commander/transfer/transfer-command.module.ts
+++ src/commander/transfer/transfer-command.module.ts
@@ -1,11 +1,26 @@
+import { BalanceService } from '@/balance/balance.service'
 import { TransferCommand } from '@/commander/transfer/transfer.command'
 import { KmsService } from '@/kms/kms.service'
 import { SignerKmsService } from '@/sign/signer-kms.service'
 import { KernelAccountClientService } from '@/transaction/smart-wallets/kernel/kernel-account-client.service'
+import { CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
 
 @Module({
-  providers: [TransferCommand, KmsService, SignerKmsService, KernelAccountClientService],
-  exports: [TransferCommand, KmsService, SignerKmsService, KernelAccountClientService],
+  imports: [CacheModule.register()],
+  providers: [
+    TransferCommand,
+    KmsService,
+    SignerKmsService,
+    KernelAccountClientService,
+    BalanceService,
+  ],
+  exports: [
+    TransferCommand,
+    KmsService,
+    SignerKmsService,
+    KernelAccountClientService,
+    BalanceService,
+  ],
 })
 export class TransferCommandModule {}
--- src/commander/transfer/transfer.command.ts
+++ src/commander/transfer/transfer.command.ts
@@ -1,6 +1,7 @@
-import { encodeFunctionData, getAddress, Hex, erc20Abi } from 'viem'
+import { encodeFunctionData, getAddress, Hex, erc20Abi, parseEther } from 'viem'
 import { Command, CommandRunner, Option } from 'nest-commander'
 import { KernelAccountClientService } from '@/transaction/smart-wallets/kernel/kernel-account-client.service'
+import { BalanceService } from '@/balance/balance.service'
 
 @Command({
   name: 'transfer',
@@ -9,7 +10,10 @@ import { KernelAccountClientService } from '@/transaction/smart-wallets/kernel/k
     'Moves ERC20 tokens from the Kernel wallet to another address, all values are in decimal 6 format',
 })
 export class TransferCommand extends CommandRunner {
-  constructor(private readonly kernelAccountClientService: KernelAccountClientService) {
+  constructor(
+    private readonly kernelAccountClientService: KernelAccountClientService,
+    private readonly balanceService: BalanceService,
+  ) {
     super()
   }
 
@@ -17,11 +21,25 @@ export class TransferCommand extends CommandRunner {
     console.log('CLI TransferCommand Params', passedParams)
     const recipient = getAddress(passedParams[0])
     console.log('Recipient', recipient)
+
+    if (options?.native && options?.chainID) {
+      console.log(`Transfering native tokens to ${recipient} with amount: ${options.native}`)
+      await this.transferNative(options.chainID, recipient, options.native)
+      return
+    }
+
+    if (options?.everything && options?.chainID) {
+      console.log(`Transfering all tokens to ${recipient}`)
+      await this.transferTokens(options.chainID, recipient)
+      return
+    }
+
     if (options?.token && options?.amount && options?.chainID) {
       console.log(
         `Transfering token: ${options.token} to ${recipient} with amount: ${options.amount}`,
       )
-      this.transferToken(options.chainID, options.token, recipient, options.amount)
+      await this.transferToken(options.chainID, options.token, recipient, options.amount)
+      return
     }
   }
 
@@ -58,6 +76,22 @@ export class TransferCommand extends CommandRunner {
     return true
   }
 
+  @Option({
+    flags: '-n, --native <native>',
+    description: 'The amount of native tokens to send, in normal eth format',
+  })
+  parseNative(val: string) {
+    console.log('parseNative', val)
+    return parseEther(val)
+  }
+
+  /**
+   * Transfers a token to a recipient
+   * @param chainID the chain id
+   * @param token the token address
+   * @param recipient the recipient address
+   * @param amount the amount to transfer, assumes in correct decimal format for that token
+   */
   async transferToken(chainID: number, token: Hex, recipient: Hex, amount: bigint) {
     const client = await this.kernelAccountClientService.getClient(chainID)
     const transferFunctionData = encodeFunctionData({
@@ -68,6 +102,36 @@ export class TransferCommand extends CommandRunner {
     const receipt = await client.execute([
       { to: token, data: transferFunctionData, value: BigInt(0) },
     ])
-    console.log('Transfer Request', receipt)
+    console.log('Transfer Receipt', receipt)
+  }
+
+  /**
+   * Sends all the tokens on a given chain to a recipient
+   * @param chainID the chain id
+   * @param recipient the recipient address
+   * @returns 
+   */
+  async transferTokens(chainID: number, recipient: Hex) {
+    const tokens = await this.balanceService.fetchTokenBalancesForChain(chainID)
+    if (!tokens) {
+      console.log('No tokens found')
+      return
+    }
+    const filtered = Object.values(tokens).filter((token) => token.balance > BigInt(0))
+    for (const token of filtered) {
+      await this.transferToken(chainID, token.address, recipient, token.balance)
+    }
+  }
+
+  /**
+   * Transfers native tokens to a recipient
+   * @param chainID the chain id
+   * @param recipient the recipient address
+   * @param amount the amount to transfer in wei format
+   */
+  async transferNative(chainID: number, recipient: Hex, amount: bigint) {
+    const client = await this.kernelAccountClientService.getClient(chainID)
+    const receipt = await client.execute([{ to: recipient, value: amount }])
+    console.log('Transfer Receipt', receipt)
   }
 }
