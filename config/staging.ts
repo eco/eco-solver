bac67bd9092b3742a7ccea9941f91cd805feab52
TylerGlenski <40610184+TylerGlenski@users.noreply.github.com>
2025-03-24 15:11:53 -0500

    added whitelist secretname to staging.ts config (#115)
    Co-authored-by: tylerglenski <tglenski@gmail.com>

--- config/staging.ts
+++ config/staging.ts
@@ -8,6 +8,10 @@ export default {
       region: 'us-east-2',
       secretID: 'eco-solver-configs-staging',
     },
+    {
+      region: 'us-east-2',
+      secretID: 'eco-solver-whitelist-staging',
+    },
   ],
   //don't add anything else here
 }
