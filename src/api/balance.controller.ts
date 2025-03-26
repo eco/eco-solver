bfb70f58ced97d906637761301bd8d2e3305ae51
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-03-25 20:34:08 -0400

    adding flatten query param and tests

--- src/api/balance.controller.ts
+++ src/api/balance.controller.ts
@@ -1,8 +1,10 @@
 import { BalanceService } from '@/balance/balance.service'
+import { TokenBalance, TokenConfig } from '@/balance/types'
 import { API_ROOT, BALANCE_ROUTE } from '@/common/routes/constants'
 import { convertBigIntsToStrings } from '@/common/viem/utils'
 import { CacheInterceptor } from '@nestjs/cache-manager'
-import { Controller, Get, UseInterceptors } from '@nestjs/common'
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common'
+import * as _ from 'lodash'
 
 @Controller(API_ROOT + BALANCE_ROUTE)
 @UseInterceptors(CacheInterceptor)
@@ -10,7 +12,30 @@ export class BalanceController {
   constructor(private readonly balanceService: BalanceService) {}
 
   @Get()
-  async getBalances() {
-    return convertBigIntsToStrings(await this.balanceService.getAllTokenData())
+  async getBalances(@Query('flat') flat?: boolean) {
+    const data = await this.balanceService.getAllTokenData()
+    if (flat) {
+      return convertBigIntsToStrings(this.groupTokensByChain(data))
+    }
+    return convertBigIntsToStrings(data)
+  }
+
+  groupTokensByChain(
+    data: {
+      config: TokenConfig
+      balance: TokenBalance
+      chainId: number
+    }[],
+  ) {
+    // Group tokens by the chainId property.
+    const grouped = _.groupBy(data, 'chainId')
+
+    // For each chainId group, map the tokens to only include token address and balance.
+    return _.mapValues(grouped, (tokens) =>
+      tokens.map((token) => ({
+        address: token.balance.address,
+        balance: token.balance.balance,
+      })),
+    )
   }
 }
