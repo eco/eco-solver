e151f00dc045c61eee826568eafd48e4922e5c33
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-03-25 20:41:23 -0400

    adding flatten query param and tests (#118)

--- src/api/tests/balance.controller.spec.ts
+++ src/api/tests/balance.controller.spec.ts
@@ -49,5 +49,65 @@ describe('BalanceController Test', () => {
 
       expect(getAllTokenDataSpy).toHaveBeenCalled()
     })
+
+    it('should take the flat query param', async () => {
+      const getAllTokenDataSpy = jest.spyOn(balanceService, 'getAllTokenData').mockResolvedValue([])
+      const groupSpy = jest.spyOn(balanceController, 'groupTokensByChain')
+      await balanceController.getBalances(true)
+
+      expect(getAllTokenDataSpy).toHaveBeenCalled()
+      expect(groupSpy).toHaveBeenCalled()
+    })
+  })
+
+  describe('groupTokensByChain', () => {
+    it('should flatten', async () => {
+      const data = [
+        {
+          balance: {
+            address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+            balance: '50995350000',
+            decimals: 6,
+          },
+          chainId: 1,
+        },
+        {
+          config: {
+            address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+            chainId: 1,
+          },
+          balance: {
+            address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+            balance: '50000000000',
+            decimals: 6,
+          },
+          chainId: 1,
+        },
+        {
+          config: {
+            address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+            chainId: 42161,
+            type: 'erc20',
+            minBalance: 200,
+            targetBalance: 50000,
+          },
+          balance: {
+            address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+            balance: '25368636844',
+            decimals: 6,
+          },
+          chainId: 42161,
+        },
+      ] as any
+
+      const result = balanceController.groupTokensByChain(data)
+      expect(result).toEqual({
+        1: [
+          { address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', balance: '50995350000' },
+          { address: '0xdAC17F958D2ee523a2206206994597C13D831ec7', balance: '50000000000' },
+        ],
+        42161: [{ address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', balance: '25368636844' }],
+      })
+    })
   })
 })
