45c46761413cca370c62650c76ef464b5b47350b
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-04-04 14:31:26 -0400

    fix celo to use alchemy

--- src/common/chains/definitions/celo.ts
+++ src/common/chains/definitions/celo.ts
@@ -4,9 +4,9 @@ import { celo as vcelo } from 'viem/chains'
 export const celo: Chain = {
   ...vcelo,
   rpcUrls: {
-    default: {
-      http: [`https://responsive-lingering-yard.celo-mainnet.quiknode.pro/{QUICKNODE_API_KEY}`],
-      webSocket: [`wss://responsive-lingering-yard.celo-mainnet.quiknode.pro/{QUICKNODE_API_KEY}`],
+      ...vcelo.rpcUrls,
+    alchemy: {
+      http: ['https://celo-mainnet.g.alchemy.com/v2'],
     },
   },
 }
