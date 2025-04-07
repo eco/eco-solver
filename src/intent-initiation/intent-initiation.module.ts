c8dc74240555bf4482202d4fbb682dc9d9d858df
aleph2012 <extropy@gmail.com>
2025-04-07 18:38:08 +0200

    - Moved IntentInitiationController to api module

--- src/intent-initiation/intent-initiation.module.ts
+++ src/intent-initiation/intent-initiation.module.ts
@@ -1,11 +1,10 @@
-import { IntentInitiationController } from '@/intent-initiation/controllers/intent-initiation.controller'
 import { IntentInitiationService } from '@/intent-initiation/services/intent-initiation.service'
 import { Module } from '@nestjs/common'
 
 @Module({
   imports: [],
 
-  controllers: [IntentInitiationController],
+  controllers: [],
 
   providers: [IntentInitiationService],
 
