439cf7de88a52ad380f57fc66c5565dc35d2d41e
Carlos Febres <cfebres@eco.com>
2025-04-25 14:09:47 -0400

    import the RequestSigningModule module in the SolverRegistrationModule

--- src/solver-registration/solver-registration.module.ts
+++ src/solver-registration/solver-registration.module.ts
@@ -1,9 +1,11 @@
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
 import { SolverRegistrationService } from '@/solver-registration/services/solver-registration.service'
+import { RequestSigningModule } from '@/request-signing/request-signing.module'
 
 @Module({
   imports: [
+    RequestSigningModule,
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
