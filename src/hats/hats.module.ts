2458228cc81c08f4739314f53fbf654736d8702b
Dirk Page <dpage@eco.com>
2025-05-05 10:21:11 -0500

    Removing ecoconfig module import from hats module

--- src/hats/hats.module.ts
+++ src/hats/hats.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HatsService } from './hats.service';
-import { EcoConfigModule } from '@/eco-configs/eco-config.module';
 import { BalanceModule } from '@/balance/balance.module';
 import { TransactionModule } from '@/transaction/transaction.module';
 
 @Module({
-  imports: [EcoConfigModule, BalanceModule, TransactionModule],
+  imports: [BalanceModule, TransactionModule],
 
   providers: [HatsService],
 
