fa6c0478ea4b1260d67954739cb756c30a03e155
aleph2012 <extropy@gmail.com>
2025-05-14 18:57:36 +0200

    - Use proper enum for ProofType  - rebased

--- src/contracts/prover.ts
+++ src/contracts/prover.ts
@@ -1,9 +1,29 @@
-export const PROOF_HYPERLANE = 'Hyperlane'
-export const PROOF_METALAYER = 'Metalayer'
+import { Enumify } from 'enumify'
 
-export type ProofType = typeof PROOF_HYPERLANE | typeof PROOF_METALAYER
+export class ProofType extends Enumify {
+  static HYPERLANE = new ProofType()
+  static METALAYER = new ProofType()
+  static _ = ProofType.closeEnum()
 
-export const Proofs: Record<string, ProofType> = {
-  Hyperlane: PROOF_HYPERLANE,
-  Metalayer: PROOF_METALAYER,
+  static fromString(enumstr: string): ProofType {
+    const value = ProofType.enumValueOf(enumstr)
+
+    if (value) {
+      return value as ProofType
+    }
+
+    throw new Error(`Proof type ${enumstr} is not supported`)
+  }
+
+  isHyperlane(): boolean {
+    return this === ProofType.HYPERLANE
+  }
+
+  isMetalayer(): boolean {
+    return this === ProofType.METALAYER
+  }
+
+  toString() {
+    return this.enumKey
+  }
 }
