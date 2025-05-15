80c822ce71eae8340a26a85b778e073bf5b27bb4
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-05-15 17:04:08 -0400

    updating the supported fetch to get it from the eco-foundation/chains npm package - rebased

--- src/common/chains/supported.ts
+++ src/common/chains/supported.ts
@@ -1,33 +1,13 @@
+import { EcoRoutesChainDefinitions } from '@eco-foundation/chains'
 import { anvil } from './definitions/anvil'
-import { arbitrum } from './definitions/arbitrum'
-import { base, baseSepolia } from './definitions/base'
-import { celo, ink } from 'viem/chains'
+
 import { Chain } from 'viem'
-import { ecoSepolia } from './definitions/eco'
-import { ethereum, sepolia } from './definitions/ethereum'
-import { helix } from './definitions/helix'
-import { mantle } from './definitions/mantle'
-import { optimism, optimismSepolia } from './definitions/optimism'
-import { polygon } from './definitions/polygon'
-import { unichain } from './definitions/unichain'
+
 
 /**
  * List of supported chains for the solver that have modified RPC URLs or are defined in the project
  */
 export const ChainsSupported: Chain[] = [
   anvil,
-  optimism,
-  optimismSepolia,
-  base,
-  baseSepolia,
-  ecoSepolia,
-  helix,
-  arbitrum,
-  mantle,
-  polygon,
-  ethereum,
-  sepolia,
-  celo,
-  ink,
-  unichain,
+  ...Object.values(EcoRoutesChainDefinitions as any) as any
 ]
