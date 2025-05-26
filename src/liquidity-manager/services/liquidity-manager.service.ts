f262439e92603ce16f65a48fc01db32df9d2f421
Carlos Febres <cfebres@eco.com>
2025-05-26 02:26:35 -0400

    feat: turn on/off liquidity manager

--- src/liquidity-manager/services/liquidity-manager.service.ts
+++ src/liquidity-manager/services/liquidity-manager.service.ts
@@ -69,6 +69,12 @@ export class LiquidityManagerService implements OnApplicationBootstrap {
     // Get wallet addresses we'll be monitoring
     this.config = this.ecoConfigService.getLiquidityManager()
 
+    if (this.config.enabled !== false) {
+      await this.initializeRebalances()
+    }
+  }
+
+  async initializeRebalances() {
     // Use OP as the default chain assuming the Kernel wallet is the same across all chains
     const opChainId = 10
     const client = await this.kernelAccountClientService.getClient(opChainId)
