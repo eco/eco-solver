f5af4d27bbfa3c3ca3876af319f63564c4f708d1
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-05-29 11:53:53 -0400

    Revert "adding quite option for lifi" - rebased This reverts commit fe72394e897c61771f321df3c4e859fce167febb.

--- src/liquidity-manager/services/liquidity-providers/LiFi/utils/get-transaction-hashes.ts
+++ src/liquidity-manager/services/liquidity-providers/LiFi/utils/get-transaction-hashes.ts
@@ -5,7 +5,7 @@ import { EcoLogMessage } from '@/common/logging/eco-log-message'
 export function logLiFiProcess(logger: Logger, route: RouteExtended) {
   route.steps.forEach((step, index) => {
     step.execution?.process.forEach((process) => {
-      logger.debug(
+      logger.log(
         EcoLogMessage.fromDefault({
           message: `LiFi: Step ${index + 1}, Process ${process.type}:`,
           properties: process,
