eccfaf5574dee9638a50508cfda65915ab917555
aleph2012 <extropy@gmail.com>
2025-07-23 20:12:11 +0200

    - Fixed staging config for BG

--- config/staging.ts
+++ config/staging.ts
@@ -1,27 +1,70 @@
+/* eslint-disable prettier/prettier */
 export default {
   server: {
-    url: 'https://solver.staging.bend.eco',
+    // url: 'https://solver.staging.bend.eco',
+    url: 'https://solver-bg.staging.eco.com',
   },
+
+  // aws: [
+  //   {
+  //     region: 'us-east-2',
+  //     secretID: 'eco-solver-secrets-staging',
+  //   },
+  //   {
+  //     region: 'us-east-2',
+  //     secretID: 'eco-solver-configs-staging',
+  //   },
+  //   {
+  //     region: 'us-east-2',
+  //     secretID: 'eco-solver-whitelist-staging',
+  //   },
+  // ],
+
+/*
+    {
+      region: 'us-east-2',
+      secretID: 'eco-solver-secrets-pre-prod',
+    },
+    {
+      region: 'us-east-2',
+      secretID: 'eco-solver-configs-pre-prod',
+    },
+    {
+      region: 'us-east-2',
+      secretID: 'eco-solver-configs-chains-preprod',
+    },
+    {
+      region: 'us-east-2',
+      secretID: 'eco-solver-whitelist-pre-prod',
+    },
+
+*/
 
   aws: [
     {
       region: 'us-east-2',
-      secretID: 'eco-solver-secrets-staging',
+      secretID: 'solver-secrets-staging-bg',
     },
     {
       region: 'us-east-2',
-      secretID: 'eco-solver-configs-staging',
+      secretID: 'solver-configs-staging-bg',
     },
     {
       region: 'us-east-2',
-      secretID: 'eco-solver-whitelist-staging',
+      secretID: 'solver-configs-chains-staging-bg',
+    },
+    {
+      region: 'us-east-2',
+      secretID: 'solver-whitelist-staging-bg',
     },
   ],
+
   //don't add anything else here
 
   solverRegistrationConfig: {
     apiOptions: {
-      baseUrl: 'https://quotes-preprod.eco.com',
+      // baseUrl: 'https://quotes-preprod.eco.com',
+      baseUrl: 'https://quotes-bg.staging.eco.com',
     },
   },
 }
