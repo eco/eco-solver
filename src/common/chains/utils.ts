4239e9def3a12fb4754011acae68ce0d5c8bce64
fernandofcampos <fernandofcampos@gmail.com>
2025-08-28 10:52:37 -0300

    Revert "Improving robustness of rpc urls (#240)" (#262)
    This reverts commit 03cb1ef10018987e452988d30c0d86d53e6cc087.
    
    Co-authored-by: aromanoEco <99753227+aromanoEco@users.noreply.github.com>

--- src/common/chains/utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Checks if a URL is a WebSocket URL.
- * @param url - The URL to check.
- * @returns True if the URL is a WebSocket URL, false otherwise.
- */
-export function isWebsocket(url: string): boolean {
-  return url.startsWith('ws://') || url.startsWith('wss://')
-}
