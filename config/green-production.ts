bce4806f28c1dfe447d9ee6b954afcb426b8e00d
tylerglenski <tglenski@gmail.com>
2025-09-12 14:26:59 -0500

    added purple yhellow green env ts config files

---
+++ config/green-production.ts
@@ -0,0 +1,35 @@
+export default {
+  server: {
+    url: process.env.SERVICE_URL || 'https://solver-green.eco.com',
+  },
+
+  indexer: {
+    url: process.env.INDEXER_URL || 'https://protocol-indexer-production.up.railway.app',
+  },
+
+  aws: [
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_MISC || 'solver-green-production',
+    },
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_CONFIGS || 'solver-configs-green-production',
+    },
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_CHAINS || 'solver-configs-chains-green-production',
+    },
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_WHITELIST || 'solver-whitelist-green-production',
+    },
+  ],
+  //don't add anything else here
+
+  solverRegistrationConfig: {
+    apiOptions: {
+      baseUrl: process.env.REGISTRATION_URL || 'https://quotes-green.eco.com',
+    },
+  },
+}
