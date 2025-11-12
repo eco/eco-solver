829cc3c2f95af81b59e964c66aac796d6f41ddb7
aleph2012 <extropy@gmail.com>
2025-11-12 17:48:49 +0100

    - Updated with LIFI branch

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,16 +1,17 @@
 import { APP_INTERCEPTOR } from '@nestjs/core'
 import { BalanceController } from '@/api/balance.controller'
 import { BalanceModule } from '@/balance/balance.module'
+import { BlockchainController } from './blockchain.controller'
+import { BlockchainModule } from '@/blockchain/blockchain.module'
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager'
 import { EcoConfigService } from '@/eco-configs/eco-config.service'
 import { IntentInitiationController } from '@/api/intent-initiation.controller'
 import { IntentInitiationModule } from '@/intent-initiation/intent-initiation.module'
+import { IntentInitiationV2Controller } from '@/api/intent-initiation-v2.controller'
 import { Module } from '@nestjs/common'
 import { QuoteController } from '@/api/quote.controller'
 import { QuoteModule } from '@/quote/quote.module'
 import { QuoteV2Controller } from '@/api/quote-v2.controller'
-import { BlockchainController } from './blockchain.controller'
-import { BlockchainModule } from '@/blockchain/blockchain.module'
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { BlockchainModule } from '@/blockchain/blockchain.module'
     QuoteController,
     QuoteV2Controller,
     IntentInitiationController,
+    IntentInitiationV2Controller,
     BlockchainController,
   ],
 
