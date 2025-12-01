57255b6ce9b2483bc4050024ec419cad996b2e6e
tylerglenski <tglenski@gmail.com>
2025-12-01 16:52:29 -0600

    v1 magenta deploy test

---
+++ config/magenta-v1-production.ts
@@ -0,0 +1,35 @@
+export default {
+  server: {
+    url: process.env.SERVICE_URL || 'https://solver-v1-magenta.eco.com',
+  },
+
+  indexer: {
+    url: process.env.INDEXER_URL || 'https://protocol-indexer-production.up.railway.app',
+  },
+
+  aws: [
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_MISC || 'solver-v1-magenta',
+    },
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_CONFIGS || 'solver-v1-configs-magenta',
+    },
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_CHAINS || 'solver-v1-configs-chains-magenta',
+    },
+    {
+      region: 'us-east-2',
+      secretID: process.env.AWS_SECRET_ID_WHITELIST || 'solver-v1-whitelist-magenta',
+    },
+  ],
+  //don't add anything else here
+
+  // solverRegistrationConfig: {
+  //   apiOptions: {
+  //     baseUrl: process.env.REGISTRATION_URL || 'https://quotes-magenta.eco.com',
+  //   },
+  // },
+}
